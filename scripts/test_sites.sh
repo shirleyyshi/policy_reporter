b7b9dc0b394b94c1184717a5052ea4d9620e4c03
石雨辰 <14227055+shirleyyyshi@user.noreply.gitee.com>
2026-08-19 17:20:15 +0800

    chore: 测试8城x4站共32个地方站点可达性

--- scripts/test_sites.sh
+++ scripts/test_sites.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
-# 测试上海 4 个政府站点从新加坡服务器的可达性
+# 测试 8 个热门城市 × 4 个委办局共 32 个政府站点从新加坡服务器的可达性
 # 中央 4 站(财政部/税务总局/央行/商务部)已确认可达，本脚本只测地方
 # 用法：bash scripts/test_sites.sh
 
-echo "=== 测试上海地方站点可达性 ==="
+echo "=== 测试 8 城地方站点可达性 ==="
 echo ""
 
+# 格式：城市|财政|税务|金融|商务
 sites=(
-    "上海市财政局|https://czj.sh.gov.cn/"
-    "上海市税务局|https://shanghai.chinatax.gov.cn/"
-    "上海市地方金融局|https://jrj.sh.gov.cn/"
-    "上海市商务委|http://swt.sh.gov.cn/"
+    # 北京
+    "北京-财政|https://czj.beijing.gov.cn/"
+    "北京-税务|http://beijing.chinatax.gov.cn/"
+    "北京-金融|https://jrj.beijing.gov.cn/"
+    "北京-商务|https://sw.beijing.gov.cn/"
+    # 上海
+    "上海-财政|https://czj.sh.gov.cn/"
+    "上海-税务|https://shanghai.chinatax.gov.cn/"
+    "上海-金融|https://jrj.sh.gov.cn/"
+    "上海-商务|http://swt.sh.gov.cn/"
+    # 天津
+    "天津-财政|https://czj.tj.gov.cn/"
+    "天津-税务|http://tianjin.chinatax.gov.cn/"
+    "天津-金融|https://jrj.tj.gov.cn/"
+    "天津-商务|http://swj.tj.gov.cn/"
+    # 重庆
+    "重庆-财政|https://czj.cq.gov.cn/"
+    "重庆-税务|http://chongqing.chinatax.gov.cn/"
+    "重庆-金融|https://jrj.cq.gov.cn/"
+    "重庆-商务|https://sww.cq.gov.cn/"
+    # 广州
+    "广州-财政|https://czj.gz.gov.cn/"
+    "广州-税务|http://guangdong.chinatax.gov.cn/"
+    "广州-金融|http://jrjgj.gz.gov.cn/"
+    "广州-商务|http://swj.gz.gov.cn/"
+    # 深圳
+    "深圳-财政|https://szfb.sz.gov.cn/"
+    "深圳-税务|https://shenzhen.chinatax.gov.cn/"
+    "深圳-金融|https://jr.sz.gov.cn/"
+    "深圳-商务|https://commerce.sz.gov.cn/"
+    # 杭州
+    "杭州-财政|https://czj.hangzhou.gov.cn/"
+    "杭州-税务|http://zhejiang.chinatax.gov.cn/"
+    "杭州-金融|https://jrj.hangzhou.gov.cn/"
+    "杭州-商务|https://sww.hangzhou.gov.cn/"
+    # 南京
+    "南京-财政|https://czj.nanjing.gov.cn/"
+    "南京-税务|http://jiangsu.chinatax.gov.cn/"
+    "南京-金融|https://jrb.nanjing.gov.cn/"
+    "南京-商务|https://swj.nanjing.gov.cn/"
 )
 
 for site in "${sites[@]}"; do
